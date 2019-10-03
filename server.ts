5902e49d91d65fc0f9bc7d40b211b62998ccebb5
jamesbyrne89 <mail@jamestbyrne.com>
2019-10-03 23:12:20 +0100

    Setup server

---
+++ server.ts
@@ -0,0 +1,17 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: true
+  })
+);
+
+app.listen(PORT, () => `Server running on port ${PORT}`);
