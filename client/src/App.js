c5fbb3cb3a4561e452b7f743120935c1caafbf24
jamesbyrne89 <mail@jamestbyrne.com>
2019-10-18 19:03:51 +0100

    Add first query to client

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import gql from 'graphql-tag';
 import ApolloClient from 'apollo-boost';
-import {ApolloProvider, Query} from 'react-apollo';
+import { ApolloProvider, Query } from 'react-apollo';
 import Select from 'react-select';
 import logo from './logo.svg';
 import './App.css';
 
 
 const client = new ApolloClient({
-  uri: '/countries'
-})
+  uri: 'http://localhost:4000/countries'
+});
+
+const COUNTRIES_QUERY = gql`
+  query countries {
+    name
+  }
+`
 
 const App = () => {
 
@@ -30,6 +37,9 @@ const options = [
       <main>
         <h1>Countries</h1>
       <div className="container">
+        <Query query={COUNTRIES_QUERY}>
+{({loading, error, data}) => console.log(data) || loading && <span>loading...</span>}
+        </Query>
         <div><span>What is the</span></div>
         <Select className="options-dropdown country-property-dropdown" options={options}/>
         <div><span>of</span></div>
